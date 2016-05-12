d65119277f983ebb78d22698db4a422006df6d4a
U-DATATEH\gkasymov <kasymovga@xsmail.com>
2016-05-12 12:14:06 +0500

    GUI improved.

--- RLGUI.java
+++ RLGUI.java
@@ -4,15 +4,17 @@ import java.awt.*;
 
 public class RLGUI extends JFrame {
 	private JLabel statusLabel;
-	private JLabel infoLabel;
+	private JProgressBar progressBar;
 	public RLGUI() {
 		this.setTitle("Rexuiz Launcher");
-		statusLabel = new JLabel("Preparing to launch...");
-		infoLabel = new JLabel("...");
+		statusLabel = new JLabel("Preparing to launch...", SwingConstants.CENTER);
+		progressBar = new JProgressBar(0, 100);
+		progressBar.setStringPainted(true);
 		this.setLayout(new GridLayout(2, 2));
-        this.getContentPane().add(statusLabel);
-        this.getContentPane().add(infoLabel);
+		this.getContentPane().add(statusLabel);
+		this.getContentPane().add(progressBar);
 		this.pack();
+		this.setSize(320, 120);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	public void run() {
@@ -25,7 +27,8 @@ public class RLGUI extends JFrame {
 		statusLabel.setText(message);
 	}
 	public void progress(double f) {
-		infoLabel.setText(String.format("%3.1f%%", f * 100));
+		progressBar.setValue((int)(f * 100));
+		progressBar.setString(String.format("%3.1f%%", f * 100));
 	}
 	public boolean ask(String question) {
 		JFrame frame = new JFrame();
