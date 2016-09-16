0dc5b2466c484bef3d880817c35353f83be344c7
Grigorii Kasymov <kasymovga@xsmail.com>
2016-09-16 11:38:42 +0500

    Icon and logo in GUI

--- src/com/rexuiz/gui/GraphicalUserInterface.java
+++ src/com/rexuiz/gui/GraphicalUserInterface.java
@@ -2,6 +2,9 @@ package com.rexuiz.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
 
 public class GraphicalUserInterface extends JFrame {
 	private JLabel statusLabel;
@@ -14,13 +17,34 @@ public class GraphicalUserInterface extends JFrame {
 		statusLabel = new JLabel("Preparing to launch...", SwingConstants.CENTER);
 		progressBar = new JProgressBar(0, 100);
 		progressBar.setStringPainted(true);
-		setLayout(new GridLayout(2, 2));
+		setLayout(new GridLayout(3, 1));
+		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("icon.png");
+		if (input !=  null) {
+			try {
+				BufferedImage icon = ImageIO.read(input);
+				if (icon !=  null)
+					setIconImage(icon);
+			} catch (Exception ex) {
+				//ignore
+			}
+		}
+		input = Thread.currentThread().getContextClassLoader().getResourceAsStream("logo.png");
+		if (input !=  null) {
+			try {
+				BufferedImage logo = ImageIO.read(input);
+				if (logo !=  null)
+					getContentPane().add(new JLabel(new ImageIcon(logo)));
+			} catch (Exception ex) {
+				//ignore
+			}
+		}
 		getContentPane().add(statusLabel);
 		getContentPane().add(progressBar);
 		pack();
 		setSize(320, 120);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+
 	}
 	public void showMainDialog() {
 		this.setVisible(true);
