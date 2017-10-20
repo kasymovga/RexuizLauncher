f4d411bb88b7167c94990cb2299acefbbaf9344e
Arthur Aukhatov <aaukhatov@gmail.com>
2017-10-21 00:35:05 +0500

    refactoring checkFile method, and extract the method to util class

--- src/main/java/com/rexuiz/utils/DigestUtils.java
+++ src/main/java/com/rexuiz/utils/DigestUtils.java
@@ -1,5 +1,14 @@
 package com.rexuiz.utils;
 
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class DigestUtils {
 
 	private static final char[] hexCode = "0123456789ABCDEF".toCharArray();
@@ -11,5 +20,29 @@ public class DigestUtils {
 			r.append(hexCode[(b & 0xF)]);
 		}
 		return r.toString();
+	}
+
+	public static boolean checkFile(String path, String hash, long size)
+			throws IOException, NoSuchAlgorithmException {
+		Path nioPath = Paths.get(path);
+		long fileSize = Files.size(nioPath);
+
+		if (fileSize != size) {
+			return false;
+		}
+
+		MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+
+		try (FileChannel channel = (FileChannel) Files.newByteChannel(nioPath)) {
+			MappedByteBuffer byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+			messageDigest.update(byteBuffer);
+		} catch (IOException ex) {
+			System.out.println(ex.getLocalizedMessage());
+		}
+
+		byte[] digest = messageDigest.digest();
+		String hashReal = printHexBinary(digest).toLowerCase();
+
+		return MessageDigest.isEqual(hashReal.getBytes(), hash.getBytes());
 	}
 }
